a2785f275d681d7df9b30f69ab5c3b57940a2420
jinjiaxing <jinjiaxing@baidu.com>
2018-04-18 18:08:07 +0800

    version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # react-te-cli
 react-cli 工具，通过一条命令 创建完整的基于 react redux react-router webpack 的项目模板， 直接coding
+
+## how to use?
+react-te-cli appName
+
--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,28 @@
 #!/usr/bin/env node
 
 console.log('welcome to react-te-cli');
+
+const clone = require('git-clone');
+const program = require('commander');
+const shell = require('shelljs');
+const log = require('tracer').colorConsole();
+
+program
+    .version('1.0.0')
+    .description('react工程化模板cli工具')
+program
+    .command('* <project>')
+    .action(function(project) {
+        log.info('使用例子：react-te-cli projectName')
+        if (project) {
+            let pwd = shell.pwd()
+            log.info(`正在拉取模板代码，下载位置：${pwd}/${project}/ ...`)
+            clone(`https://github.com/jinjiaxing/react-template-easily.git`, pwd + `/${project}`, null, function() {
+                shell.rm('-rf', pwd + `/${project}/.git`)
+                log.info('模板工程建立完成')
+            })
+        } else {
+            log.error('正确命令例子：react-te-cli projectName')
+        }
+    })
+program.parse(process.argv)
